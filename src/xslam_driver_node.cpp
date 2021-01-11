64b26c2fd3b4fcb83ad0ace6404a543b1c889703
Yeah2333 <653091941@qq.com>
2021-01-11 17:03:26 +0800

    init

---
+++ src/xslam_driver_node.cpp
@@ -0,0 +1,17 @@
+//
+// Created by wangzhiyong on 2021/1/11.
+//
+#include <ros/ros.h>
+#include <iostream>
+#include <iomanip>
+
+#include <xslam/xslam_sdk.hpp>
+
+#include <cmath>
+
+auto odom_pub = ros::publish
+
+int main() {
+
+
+}
