b0e79e114a3a50f8dadfa31bc5b6dbfe3304cc36
MrMiilk <1024109095@qq.com>
2021-01-14 21:08:33 +0800

    add # 添加tof

--- src/xslam_driver_node.cpp
+++ src/xslam_driver_node.cpp
@@ -12,77 +12,87 @@
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
 #include <tf/tf.h>
-
+#include <sensor_msgs/image_encodings.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <memory>
+
+// convert image by its encoding
 cv::Mat rgbImage(xslam_rgb *rgb);
-
-
+// get encoding string
 std::string codec_name( xslam_rgb_codec codec );
+// dist to color
+std::tuple<int,int,int> color(double distance, double distance_min, double distance_max, double threshold);
+// formate depth image
+std::shared_ptr<unsigned char> tofImage(float *data, unsigned int width, unsigned int height, double distance);
 
+/// callback functions for VISIO camera
 void pub_pose(xslam_pose_quaternion* pose);
 void pub_rgb(xslam_rgb* rgb);
+void pub_depth(xslam_tof* tof);
 void lost(float timestamp);
 
-
+/// Publishers
 ros::Publisher odom_pub;
 image_transport::Publisher image_pub;
+image_transport::Publisher depth_pub;
+
+
+
 
 int main(int argc, char** argv) {
-
     ros::init(argc,argv,"xslam_node");
     ros::NodeHandle nh;
 
     image_transport::ImageTransport it(nh);
     image_pub = it.advertise("xslam/image", 100);
-    // image_pub = nh.advertise<sensor_msgs::Image>("xslam/image", 100);
+    depth_pub = it.advertise("xslam/depth", 100);
     odom_pub = nh.advertise<nav_msgs::Odometry>("xslam/odom", 10000);
 
-    xslam_disp_version();
 
+    xslam_disp_version();
+    // ====================================
+    /// set callback functions for VISIO camera
+    // ====================================
     xslam_6dof_quaternion_callback(&pub_pose);
     xslam_rgb_callback(&pub_rgb);
+    xslam_tof_callback(&pub_depth);
     xslam_lost_callback(&lost);
-
-    // start visual odometry
-    xslam_start_vo();
-    xslam_start_camera();
-    // RGB_640x480      RGB_1280x720        RGB_1920x1080
+    
+    /// Configution of VISIO camera
+    // resolution       RGB_640x480      RGB_1280x720        RGB_1920x1080
     xslam_set_rgb_resolution( xslam_rgb_resolution::RGB_1920x1080);
+    // TOF      
+    // framerate:       TOF_5Hz     TOF_10Hz        TOF_20Hz        TOF_25Hz        TOF_30Hz
+    // mode:                TOF_LONG_RANGE  or  TOF_SHORT_RANGE
+    // option:              disable_tof_median_filter  or  enable_tof_median_filter
+    xslam_set_tof_framerate(TOF_10Hz);
+    xslam_set_tof_mode(TOF_LONG_RANGE);
+    // xslam_set_option(enable_tof_median_filter);
 
+    // ====================================
+    // start visual odometry
+    // ====================================
+    xslam_start_camera();
+    xslam_start_vo();
+
+    // ====================================
+    // use 'while' to control freq
+    // ====================================
     std::cout << " Press Enter to stop the process" << std::endl;
     std::cin.get();
     std::cout << " Stop process ..." << std::endl;
 
+    // ====================================
     // stop visual odometry
+    // ====================================
     xslam_stop();
-
     // free ressources
     return xslam_free() == xslam_status::failure ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
 void pub_pose(xslam_pose_quaternion* pose)
 {
-    static FrequencyCounter fc;
-    static int cnt = 0;
-
-    fc.tic();
-
-    if(cnt++ % 300 == 0)
-    {
-        double fps = fc.fps();
-        double distance_to_origin = std::sqrt(pose->x[0]*pose->x[0] + pose->x[1]*pose->x[1] + pose->x[2]*pose->x[2]);
-        std::cout
-                <<"[6DOF]: [" << std::setw(8) << pose->timestamp << std::setw(4) << " s],"
-                << " fps=" << int(fps) << std::setprecision(5)
-                << " p=(" << pose->x[0] << " " << pose->x[1] << " " << pose->x[2]
-                << " ), q=(" << pose->quaternion[0] << " " << pose->quaternion[1] << " " << pose->quaternion[2] << " " << pose->quaternion[3]
-                << " ), to origin(" << distance_to_origin << ")"
-                << ", Confidence= " << (int)pose->confidence << std::endl;
-    }
-
-    //TODO add my msg there
     nav_msgs::Odometry temp_msg;
     temp_msg.header.frame_id = "odom";
     temp_msg.header.stamp.fromSec(pose->timestamp);
@@ -97,26 +107,41 @@ void pub_pose(xslam_pose_quaternion* pose)
     odom_pub.publish(temp_msg);
 }
 
-std::string codec_name( xslam_rgb_codec codec )
-{
-    switch( codec ){
-    case YUYV: return "YUYV";
-    case YUV420p: return "YUV420p";
-    case JPEG: return "JPEG";
-    }
-    return "unknown";
-}
-
 void pub_rgb(xslam_rgb* rgb)
 {
-    // rgb->codec = xslam_rgb_codec::JPEG;
     cv::Mat image = rgbImage( rgb );
-	//将Mat类型转为sensor_msgs::Image
+	
+    //convert from type cv::Mat to sensor_msgs::Image
 	sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
     msg->header.frame_id = "image";
-	//发布消息
+    msg->header.stamp.fromSec(rgb->timestamp);
+
 	image_pub.publish(msg);
 }
+
+void pub_depth(xslam_tof* tof) {
+    std::shared_ptr<unsigned char> tmp = tofImage( tof->data, tof->width, tof->height, 2.5 );
+    cv::Mat im(tof->height, tof->width, CV_8UC3, tmp.get() );
+    
+    sensor_msgs::ImagePtr depth_msg = boost::make_shared<sensor_msgs::Image>();
+    depth_msg->header.stamp.fromSec(tof->timestamp);
+    depth_msg->encoding     = sensor_msgs::image_encodings::TYPE_8UC3;
+    depth_msg->height           =  tof->height;
+    depth_msg->width            =  tof->width;
+    depth_msg->step               = depth_msg->width * 3;   // 8UC3
+    depth_msg->data.resize(depth_msg->height * depth_msg->step);
+    memcpy(reinterpret_cast<void*>(&depth_msg->data[0]), im.data, depth_msg->height * depth_msg->step);
+    
+    depth_pub.publish(depth_msg);
+}
+
+void lost(float timestamp)
+{
+    std::cout << "[LOST] Device is lost at timestamp " << timestamp << " sec." << std::endl;
+}
+
+
+
 cv::Mat rgbImage(xslam_rgb *rgb)
 {
     cv::Mat out;
@@ -141,8 +166,39 @@ cv::Mat rgbImage(xslam_rgb *rgb)
     return out;
 }
 
-
-void lost(float timestamp)
+std::string codec_name( xslam_rgb_codec codec )
 {
-    std::cout << "[LOST] Device is lost at timestamp " << timestamp << " sec." << std::endl;
+    switch( codec ){
+    case YUYV: return "YUYV";
+    case YUV420p: return "YUV420p";
+    case JPEG: return "JPEG";
+    }
+    return "unknown";
 }
+
+std::tuple<int,int,int> color(double distance, double distance_min, double distance_max, double threshold)
+{
+    double d = std::max(distance_min, std::min( distance, distance_max ) );
+    d = (d - distance_min)/(distance_max - distance_min);
+    if( distance <= threshold || distance > 9){
+        return std::tuple<int,int,int>(0,0,0);
+    }
+    int b = static_cast<int>(255.0 * std::min(std::max(0.0, 1.5-std::abs(1.0-4.0*(d-0.5))),1.0));
+    int g = static_cast<int>(255.0 * std::min(std::max(0.0, 1.5-std::abs(1.0-4.0*(d-0.25))),1.0));
+    int r = static_cast<int>(255.0 * std::min(std::max(0.0, 1.5-std::abs(1.0-4.0*d)),1.0));
+    return std::tuple<int,int,int>(r,g,b);
+}
+
+std::shared_ptr<unsigned char> tofImage(float *data, unsigned int width, unsigned int height, double distance)
+{
+    std::shared_ptr<unsigned char> out( new unsigned char[width*height*3] );
+    for (unsigned int i=0; i<width*height; i++) {
+        auto c = color( data[i], 0.0f, distance, 0.0f);
+        out.get()[i*3+0] = static_cast<unsigned char>(std::get<2>(c));
+        out.get()[i*3+1] = static_cast<unsigned char>(std::get<1>(c));
+        out.get()[i*3+2] = static_cast<unsigned char>(std::get<0>(c));
+    }
+    return out;
+}
+
+
